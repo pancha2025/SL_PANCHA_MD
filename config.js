54ef4ab77633f76c5b2331c06029c46523f207d6
pancha2025 <chameshkumara1@gmail.com>
2024-12-05 14:14:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "enter mongodb here",
+SESSION_ID: process.env.SESSION_ID || "KjwDVACL#zQO_rut2RJB2-zALr7cbnHz9oFnxyj8teGhC7zKO9ik",
+MONGODB: process.env.MONGODB || "mongodb://mongo:rMqkSeEtKJNSjhqkKaRSzScJoLoyxulo@junction.proxy.rlwy.net:41051",
 };
